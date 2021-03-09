9ffac0f95880baddb4794f5e3ef4b0b8c94c7ea5
rasna-skumar1306 <rasna5202@gmail.com>
2021-03-09 20:36:33 +0530

    Initialize project using Create React App

---
+++ Books.js
@@ -0,0 +1,60 @@
+const Books = [
+  {
+    BookID: 1,
+    Title: 'The Great Gatsby',
+    Author: 'F. Scott Fitgerald',
+    Price: 8.99,
+    PublishDate: '4/10/1925',
+  },
+  {
+    BookID: 2,
+    Title: 'Crime and Punishment',
+    Author: 'Fyodor Dostoyevsky',
+    Price: 12.99,
+    PublishDate: '2/12/1866',
+  },
+  {
+    BookID: 3,
+    Title: 'Don Quixote',
+    Author: 'Miguel de Cervantes',
+    Price: 6.99,
+    PublishDate: '1/15/1620',
+  },
+  {
+    BookID: 4,
+    Title: 'Jane Eyre',
+    Author: 'Charlotte Bronte',
+    Price: 9.99,
+    PublishDate: '10/16/1847',
+  },
+  {
+    BookID: 5,
+    Title: 'Moby Dick',
+    Author: 'Herman Melville',
+    Price: 4.99,
+    PublishDate: '10/18/1851',
+  },
+  {
+    BookID: 6,
+    Title: 'War and Peace',
+    Author: 'Leo Tolstoy',
+    Price: 14.99,
+    PublishDate: '8/1/1869',
+  },
+  {
+    BookID: 7,
+    Title: 'Wuthering Heights',
+    Author: 'Emily Bronte',
+    Price: 7.99,
+    PublishDate: '12/7/1847',
+  },
+  {
+    BookID: 8,
+    Title: 'The Adventures of Huckleberry Finn',
+    Author: 'Mark Twain',
+    Price: 5.99,
+    PublishDate: '12/10/1884',
+  },
+]
+
+export default Books
