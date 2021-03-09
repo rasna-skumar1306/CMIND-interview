66e28c44b6ec65b59d2d297fdefc584499d3c5d3
rasna-skumar1306 <rasna5202@gmail.com>
2021-03-09 23:27:21 +0530

    1st 3 tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
+
+import orderBy from 'lodash/orderBy'
+import Books from './Books'
 
 function App() {
+  const [columnSort, setColumnSort] = useState({ name: '', type: 'asc' })
+  const [search, setSearch] = useState('')
+  const [results, setResults] = useState(Books.rows)
+
+  useEffect(() => {
+    handleSearch()
+    //eslint-disable-next-line
+  }, [search])
+
+  const invert = {
+    asc: 'desc',
+    desc: 'asc',
+  }
+
+  const sort = (prop) => {
+    setColumnSort({
+      name: prop,
+      type: columnSort.name === prop ? invert[columnSort.type] : 'asc',
+    })
+  }
+
+  const handleSearch = () => {
+    let res = Books.rows.filter(
+      (book) =>
+        book.Author.toLowerCase().includes(search.toLowerCase()) ||
+        book.Title.toLowerCase().includes(search.toLowerCase())
+    )
+    setResults(res)
+  }
+
+  const Book = ({ data }) =>
+    data.map((book, idx) => (
+      <tr key={idx} className='row'>
+        <td className='id'>{book.BookID}</td>
+        <td className='title'>{book.Title}</td>
+        <td className='author'>{book.Author}</td>
+        <td className='price'>{book.Price}</td>
+        <td className='publishDate'>{book.PublishDate}</td>
+      </tr>
+    ))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input
+        className='search'
+        type='search'
+        placeholder='search Title or Author'
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+      />
+      <table className='books'>
+        <thead>
+          <tr>
+            <th onClick={() => sort('BookId')}>Id</th>
+            <th onClick={() => sort('Title')}>Title</th>
+            <th onClick={() => sort('Author')}>Author</th>
+            <th onClick={() => sort('Price')}>Price</th>
+            <th onClick={() => sort('PublishDate')}>PublishDate</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Book data={orderBy(results, columnSort.name, columnSort.type)} />
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Books.js
@@ -0,0 +1,94 @@
+const Books = {
+  columns: [
+    {
+      label: 'BookId',
+      field: 'BookId',
+      sort: 'asc',
+      width: 150,
+    },
+    {
+      label: 'Title',
+      field: 'Title',
+      sort: 'asc',
+      width: 270,
+    },
+    {
+      label: 'Author',
+      field: 'Author',
+      sort: 'asc',
+      width: 200,
+    },
+    {
+      label: 'Price',
+      field: 'Price',
+      sort: 'asc',
+      width: 100,
+    },
+    {
+      label: 'PublishDate',
+      field: 'PublishDate',
+      sort: 'asc',
+      width: 150,
+    },
+  ],
+  rows: [
+    {
+      BookID: 1,
+      Title: 'The Great Gatsby',
+      Author: 'F. Scott Fitgerald',
+      Price: 8.99,
+      PublishDate: '4/10/1925',
+    },
+    {
+      BookID: 2,
+      Title: 'Crime and Punishment',
+      Author: 'Fyodor Dostoyevsky',
+      Price: 12.99,
+      PublishDate: '2/12/1866',
+    },
+    {
+      BookID: 3,
+      Title: 'Don Quixote',
+      Author: 'Miguel de Cervantes',
+      Price: 6.99,
+      PublishDate: '1/15/1620',
+    },
+    {
+      BookID: 4,
+      Title: 'Jane Eyre',
+      Author: 'Charlotte Bronte',
+      Price: 9.99,
+      PublishDate: '10/16/1847',
+    },
+    {
+      BookID: 5,
+      Title: 'Moby Dick',
+      Author: 'Herman Melville',
+      Price: 4.99,
+      PublishDate: '10/18/1851',
+    },
+    {
+      BookID: 6,
+      Title: 'War and Peace',
+      Author: 'Leo Tolstoy',
+      Price: 14.99,
+      PublishDate: '8/1/1869',
+    },
+    {
+      BookID: 7,
+      Title: 'Wuthering Heights',
+      Author: 'Emily Bronte',
+      Price: 7.99,
+      PublishDate: '12/7/1847',
+    },
+    {
+      BookID: 8,
+      Title: 'The Adventures of Huckleberry Finn',
+      Author: 'Mark Twain',
+      Price: 5.99,
+      PublishDate: '12/10/1884',
+    },
+  ],
+}
+
+export default Books
